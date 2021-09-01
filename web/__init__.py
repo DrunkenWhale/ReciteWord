00069eaedeb6b2e31ff7809da7786994cd8b3576
SkyLark37 <73925628+SkyLark37@users.noreply.github.com>
2021-09-01 21:17:06 +0800

    Update __init__.py
    手动提交阿巴阿巴

--- web/__init__.py
+++ web/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 from web.config import Config
-from web.extensions import db, mail
+from web.extensions import db, mail, redis, logging_format
 from flask_cors import CORS
 from web.auth.blueprints import auth_register_blueprints
 from web.book.blueprints import book_register_blueprints
 from web.word.blueprints import word_register_blueprints
+import logging
+
+index: int = 1
+
 
 def create_app(name=__name__):
     app = Flask(name)
@@ -23,6 +27,21 @@ def register_blueprints(app: Flask):
 
 def register_extensions(app: Flask):
     mail.init_app(app)
+    redis.init_app(app)
     db.init_app(app)
     with app.app_context():
         db.create_all()
+    # 定义一个钩子 储存日志
+    # 搬砖呐 明天再说啦
+    pipe = redis.pipeline()
+
+    @app.before_request
+    def logger():
+        global index
+        pipe.set(index, logging_format(
+            request.full_path,
+            request.remote_addr,
+            request.method,
+            request.form))
+        index += 1
+        pipe.execute()
